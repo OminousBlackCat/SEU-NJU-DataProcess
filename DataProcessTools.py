946d60268e3ed827f75b46bb25179aafa37a0a9d
LostBlackCat <wxy1999814@gmail.com>
2022-09-05 16:43:15 +0800

    优化代码结构 //TODO: 补充processHeader函数

--- DataProcessTools.py
+++ DataProcessTools.py
@@ -1,7 +1,5 @@
-import numpy as np
-import struct
-import io
-import cv2
+from copy import deepcopy
+import util
 from io import BytesIO
 
 # 太阳空间望远镜科学载荷数据同步头[14,11,9,0,1,4,6,15]
@@ -10,80 +8,16 @@ head_data = [14, 11, 9, 0, 1, 4, 6, 15]
 head_pic = [5, 5, 10, 10, 5, 5, 10, 10, 1, 2, 3, 4, 5, 6, 7, 8]
 
 
-# 获取下一个字节数据
-def GetNext(fread):
-    try:
-        # 读取一个字节的内容
-        data_raw = fread.read(1)
-        # 转换为数字
-        val_tuple = struct.unpack('B', data_raw)
-        # 输出结果
-        return int(val_tuple[0])
-    except:
-        # 文件读完返回-1
-        print("文件已读完")
-        return -1
-
-
-# 获取连续多个字节数据
-# 连续读取nums个字节
-def GetData(fread, nums):
-    data = []
-    for i in range(nums):
-        # 读取字符
-        x = GetNext(fread)
-        if x == -1:
-            # 无内容用0补齐
-            data.append(0)
-            break
-        else:
-            # 输出提取内容
-            data.append(x)
-    # 不足补0
-    while len(data) < nums:
-        data.append(0)
-    # 返回内容
-    return data
-
-
-# 写二进制文件
-# filename为文件名
-def FileWrite(data, file):
-    for x in data:
-        # 打包回二进制
-        a = struct.pack('B', x)
-        # 写二进制文件
-        file.write(a)
-
-
-# 获取目标同步头对应数值
-# 同步头一位代表四个二进制位
-def GetTarget(target):
-    ans = 0
-    for i in target:
-        ans = (ans << 4) + i
-    return ans
-
-
-# 获取目标list对应值，按字节转换
-# list头代表八个二进制位
-def GetResult(target):
-    ans = 0
-    for i in target:
-        ans = (ans << 8) + i
-    return ans
-
-
 # 寻找数据帧的探头
-def FindHead(fread, head_target):
+def findFrameHead(fread, head_target):
     now = 0
     # 获取数据头的内容
-    target = GetTarget(head_target)
+    target = util.getTarget(head_target)
 
     while now != target:
         # 计算当前头内容
         now = now % (1 << (4 * (len(head_target) - 2)))
-        X = GetNext(fread)
+        X = util.getNext(fread)
         if X == -1:
             return "Error"
         now = (now << 8) + X
@@ -93,13 +27,13 @@ def FindHead(fread, head_target):
 # 寻找图片帧的开头
 # 输出图片帧开头所在的坐标第一个位置
 # 否则输出len(data)-7
-def FindPicHead(data):
+def findPicHead(data):
     n = len(data)
     # print(n)
     index = 0
     now = 0
     # 获取图片
-    target = GetTarget(head_pic)
+    target = util.getTarget(head_pic)
     # print(target)
     while index < n:
 
@@ -114,24 +48,29 @@ def FindPicHead(data):
     return n - 7
 
 
+# 解析构造header的辅助数据
+def processHeader(stream):
+    return 0
+
+
 # 对图片帧文件进行处理
-def PicWork(data, num):
+def processPicStream(data, num):
     # 提取自定义数据区
     dataHead = data[8: 8 + 280]
-    Head = BytesIO()
+    headStream = BytesIO()
     # 文件流回到开头
-    Head.seek(0)
-    FileWrite(dataHead, Head)
+    util.fileWrite(dataHead, headStream)
+    headStream.seek(0)
     # 去除自定义数据区
     data = data[8 + 278:]
     # 获取数据长度
     N = len(data)
     indexList = []
-    target = GetTarget([15, 15, 4, 15, 15, 15, 5, 1])
+    target = util.getTarget([15, 15, 4, 15, 15, 15, 5, 1])
     # 统计所有头部分
     for i in range(N - 8):
         # 找图片的开头
-        if target == GetResult(data[i:i + 4]):
+        if target == util.getResult(data[i:i + 4]):
             indexList.append(i)
     M = len(indexList)
     # print(indexList)
@@ -145,45 +84,48 @@ def PicWork(data, num):
         # 创建文件流
         FileList.append(BytesIO())
         # 写文件流
-        FileWrite(data[indexList[i]:indexList[i + 1]], FileList[i])
+        util.fileWrite(data[indexList[i]:indexList[i + 1]], FileList[i])
         # 文件流回到开头
         FileList[i].seek(0)
     # 创建文件流
     FileList.append(BytesIO())
     # 写文件流
-    FileWrite(data[indexList[M - 1]:], FileList[M - 1])
+    util.fileWrite(data[indexList[M - 1]:], FileList[M - 1])
     # 文件流回到开头
     FileList[M - 1].seek(0)
-    return Head, FileList
+    headData = processHeader(headStream)
+    return headData, FileList
 
 
 # 输入文件流 对文件流工作
 # 输出List
 # 格式 每个元素为一个文件流和一个list
 # 文件流为头文件信息 list为所有子图片的文件流
-def DataWork(fread):
+def dataWork(fread):
     # num统计读取文件数量
     num = 0
     # 记录图片帧内容
     PicData = []
     # 记录数据
     Data = []
+    # 存储所有图片信息
+    allData = []
 
     # 寻找数据帧的开头
-    while FindHead(fread, head_data) == 1:
+    while findFrameHead(fread, head_data) == 1:
         # 记录头部份
-        MainHead = GetData(fread, 8)
+        MainHead = util.getData(fread, 8)
         # 提取数据部分
-        Data = Data + GetData(fread, 2032)
+        Data = Data + util.getData(fread, 2032)
         # 记录错误控制内容
-        ErrorControl = GetData(fread, 4)
+        ErrorControl = util.getData(fread, 4)
         # 在数据帧中寻找图像帧开头，如果有输出图像帧开头的index
-        index = FindPicHead(Data)
-        # 辅助数据
+        index = findPicHead(Data)
+        # 最终输出的头部信息 应该是个字典
         headerData = None
+        # 需要拼接图像list
         picList = None
-        # 存储所有图片信息
-        allData = None
+
         # 判断有无出现数据帧开头
         if index < len(Data) - 7:
             # 记录新图像帧之前的内容
@@ -193,9 +135,9 @@ def DataWork(fread):
             # 无用内容不处理
             if num > 0:
                 # 处理图像帧内容
-                headerData, picList = PicWork(PicData, num)
+                headerData, picList = processPicStream(PicData, num)
                 # 存储图片信息
-                allData.append([headerData, picList])
+                allData.append([deepcopy(headerData), deepcopy(picList)])
             # 情况图像帧
             PicData = []
             # 编号计数
@@ -208,9 +150,9 @@ def DataWork(fread):
             # 无用内容不处理
     if num > 0:
         # 处理图像帧内容
-        headerData, picList = PicWork(PicData, num)
+        headerData, picList = processPicStream(PicData, num)
         # 存储图片信息
-        allData.append([headerData, picList])
+        allData.append([deepcopy(headerData), deepcopy(picList)])
     # 输出处理信息
     print("无数据头，解压结束")
     print("发现图片帧：" + str(num))
@@ -218,14 +160,11 @@ def DataWork(fread):
     return allData
 
 
-# 解析
-
-
 if __name__ == '__main__':
     f = BytesIO()
-    FileWrite([1, 2, 3, 4, 5, 6], f)
+    util.fileWrite([1, 2, 3, 4, 5, 6], f)
     f.seek(0)
-    print(GetData(f, 6))
+    print(util.getData(f, 6))
     # filename = 'sun_42697.dat'
     # f = open(filename, 'rb')
     # s = 0
---
+++ util.py
@@ -0,0 +1,66 @@
+import struct
+
+
+# 获取下一个字节数据
+def getNext(fread):
+    try:
+        # 读取一个字节的内容
+        data_raw = fread.read(1)
+        # 转换为数字
+        val_tuple = struct.unpack('B', data_raw)
+        # 输出结果
+        return int(val_tuple[0])
+    except BaseException as exception:
+        print(exception)
+        # 文件读完返回-1
+        print("文件已读完")
+        return -1
+
+
+# 获取连续多个字节数据
+# 连续读取nums个字节
+def getData(fread, nums):
+    data = []
+    for i in range(nums):
+        # 读取字符
+        x = getNext(fread)
+        if x == -1:
+            # 无内容用0补齐
+            data.append(0)
+            break
+        else:
+            # 输出提取内容
+            data.append(x)
+    # 不足补0
+    while len(data) < nums:
+        data.append(0)
+    # 返回内容
+    return data
+
+
+# 写二进制文件
+# filename为文件名
+def fileWrite(data, file):
+    for x in data:
+        # 打包回二进制
+        a = struct.pack('B', x)
+        # 写二进制文件
+        file.write(a)
+
+
+# 获取目标同步头对应数值
+# 同步头一位代表四个二进制位
+def getTarget(target):
+    ans = 0
+    for i in target:
+        ans = (ans << 4) + i
+    return ans
+
+
+# 获取目标list对应值，按字节转换
+# list头代表八个二进制位
+def getResult(target):
+    ans = 0
+    for i in target:
+        ans = (ans << 8) + i
+    return ans
